dae76b5c958358d262ab504c712e519da817058b
Beni Randy Harold <haroldbeni111997@gmail.com>
2023-03-28 16:44:17 +0200

    lavoro in corso

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -8,17 +8,37 @@ export default {
     <header>
         <nav>
             <div class="Logo">
-                
+
             </div>
 
-            <ul>
-                <li>Home</li>
-                <li>Prodotto</li>
-                <li>Chi siamo</li>
-                <li>Contatti</li>
+            <ul class="navbar-links">
+                <li><a class="nav-link" href="#">Home</a></li>
+                <li><a class="nav-link" href="#">Prodotti</a></li>
+                <li><a class="nav-link" href="#">Chi siamo</a></li>
+                <li><a class="nav-link" href="#">Contatti</a></li>
             </ul>
         </nav>
     </header>
 </template>
 
-<style scoped></style>
+<style scoped>
+.navbar-links {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    list-style: none;
+}
+
+.nav-link {
+    padding: .5rem 1rem;
+    color: black;
+    text-decoration: none;
+    transition: color .3s ease;
+    display: inline-block;
+}
+
+.nav-link:hover {
+    color: green;
+    text-decoration: underline;
+}
+</style>
