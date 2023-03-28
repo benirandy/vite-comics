3a3644fb243aebd95a940ef7f18b57517218dbfd
Beni Randy Harold <haroldbeni111997@gmail.com>
2023-03-28 16:31:18 +0200

    first commit

---
+++ src/components/TheFooter.vue
@@ -0,0 +1,13 @@
+<script>
+export default {
+name: "TheFooter"
+}
+</script>
+
+<template>
+
+<footer>
+    © {{ new Date().getFullYear() }} | Tutti i diritti riservati
+</footer>
+
+</template>
---
+++ src/components/TheHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    name: "TheHeader"
+}
+</script>
+
+<template>
+    <header>
+        <nav>
+            <div class="Logo">
+                
+            </div>
+
+            <ul>
+                <li>Home</li>
+                <li>Prodotto</li>
+                <li>Chi siamo</li>
+                <li>Contatti</li>
+            </ul>
+        </nav>
+    </header>
+</template>
+
+<style scoped></style>
